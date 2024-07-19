e93865a7e01e5bcf3f460b930e89bcd0ff12e6a4
koopa31 <clement.cazorla31@gmail.com>
2024-07-19 11:34:51 +0200

    code detection rosette mis au propre

--- script_zebra/rosette_detection.py
+++ script_zebra/rosette_detection.py
@@ -9,124 +9,82 @@ Version: 1.0
 It segments slice by slice in all 3 directions using Cellpose 3.0 (or any 2D cell segmentation model) and recombines the
 segmentation masks using Cellstitch to get the final 3D mask.
 """
-
-
-import os
-import time
-import argparse
-
+import cupy as cp
+from scipy import ndimage as ndi
+from skimage.io import imread, imsave
 import numpy as np
-import torch
-import tifffile
-from cellpose.models import Cellpose
-from skimage import io
+from skimage.filters import threshold_otsu
+from cupyx.scipy import ndimage as ndi
+import pyclesperanto as cle
+import cucim.skimage as cuci_ski
+from skimage.measure import regionprops
+import argparse
+import os
+from tqdm import tqdm
 
-from cellstitch.pipeline import full_stitch
-# %%
-# Plotting specifications
-from matplotlib import rcParams
-#from IPython.display import display
 
-rcParams.update({'font.size': 10})
-# %% md
+# Define a function to keep the largest component
+def keep_largest_component(binary_image):
+    # Label connected components
+    labeled_array, num_features = ndi.label(binary_image)
 
-# Definition of input parameters
+    if num_features == 0:
+        return binary_image
 
-parser = argparse.ArgumentParser(description='Parameters to set to segment zebrafish 3D embryo membranes.')
-parser.add_argument('-f', type=str, help='Folder path containing the images to segment')
-parser.add_argument('-ft', type=float, help='Flow threshold for Cellpose', default=0.4)
-parser.add_argument('-mt', type=str, help='Cellpose model', default="cyto3")
-parser.add_argument('-xyd', type=int, help='xy diameter for Cellpose Segmentation', default=250)
-parser.add_argument('-zd', type=int, help='xz and yz diameter for Cellpose Segmentation', default=200)
-parser.add_argument('-c', type=str, help='Channel to segment (wavelength)', default=561)
+    # Find the largest component
+    component_sizes = np.bincount(labeled_array.ravel())
+    component_sizes[0] = 0
+    largest_component_label = component_sizes.argmax()
+    largest_component = (labeled_array == largest_component_label)
+
+    return largest_component
+
+# Load the 3D image using CuCIM
+#image = imread("/home/clement/Images/retraining_omp_48hpf/rollingballed.tif")
+
+parser = argparse.ArgumentParser(description='Parameters to set to segment zebrafish 3D olfactive epithelium.')
+parser.add_argument('-p', type=str, help='Folder path containing the images to segment')
 
 args = parser.parse_args()
 
-folder = args.f
-xy_diameter = args.xyd
-z_diameter = args.zd
-channel = args.c
-flow_threshold = args.ft
-model_type = args.mt
+path = args.p
+output_folder = os.path.join(path, "detection_rosette")
 
-# Liste pour stocker les noms des fichiers filtrés
-images_path = []
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
-# Parcourir les fichiers dans le dossier
-for fichier in os.listdir(folder):
-    if fichier.endswith('.TIF') and channel + ' Em' in fichier:
-        images_path.append(fichier)
+fichiers_tiff = sorted([f for f in os.listdir(path) if f.endswith('.tif') or f.endswith('.tiff')
+                            or f.endswith('.TIF')])
 
-print("Images to be processed:", images_path)
+for fichier in tqdm(fichiers_tiff, "Processing images"):
+    image = imread(os.path.join(path, fichier))
 
-# Fill in on the path you would like to store the stitched mask
-output_path = os.path.join(folder, "segmentation_masks")
+    # Convert to grayscale if necessary
+    gray_image = image.copy()
 
-if os.path.exists(output_path) is False:
-    os.mkdir(output_path)
+    # Apply mean thresholding
+    thresh = threshold_otsu(gray_image, nbins=2048)
+    binary_mask = gray_image > thresh
 
+    # Remove small objects and fill holes on GPU using CuCIM
+    cleaned_mask = cuci_ski.morphology.remove_small_objects(cp.array(binary_mask), min_size=10000)
+    cleaned_mask = cuci_ski.morphology.remove_small_holes(cleaned_mask, area_threshold=5000)
 
-# %% md
-# (1). Define configs & parameters
-# %%
-# load cellpose model for backbone segmentation
-# you can also replace with any 2D segmentation model that works the best for your dataset
-flow_threshold = flow_threshold
-use_gpu = True if torch.cuda.is_available() else False
-if use_gpu is True:
-    print("Using GPU")
+    # Apply morphological closing to smooth object boundaries on GPU using CuCIM
+    scale_factor = 0.25
+    cleaned_mask = cuci_ski.transform.rescale(cleaned_mask, scale_factor)
+    cleaned_mask = ndi.binary_closing(cleaned_mask, cuci_ski.morphology.ball(15))
 
-model = Cellpose(model_type='cyto3', gpu=use_gpu)
-print("Cellpose model {} loaded".format(model_type))
+    cleaned_mask = cle.voronoi_otsu_labeling(cp.asnumpy(cleaned_mask).astype("uint8"), spot_sigma=9, outline_sigma=1)
 
-# (2). Load example pairs of raw image & ground-truth mask
-# %%
-# Process all the images of the folder
-tic = time.time()
-for i, filename in enumerate(images_path):
-    output_filename = os.path.splitext(filename)[0] + "_mask" + ".tif"
+    properties = regionprops(cleaned_mask)
 
-    # Load image & masks
-    if filename[-3:] == 'npy':  # image in .npy format
-        img = np.load(os.path.join(folder, filename))
-    elif filename[-3:] == 'tif':  # imagge in TIFF format
-        img = tifffile.imread(os.path.join(folder, filename))
-    else:
-        try:
-            img = io.imread(os.path.join(folder, filename))
-        except:
-            raise IOError('Failed to load image {}'.format(filename))
+    sorted_regions = sorted(properties, key=lambda x: x.extent, reverse=True)
 
-    # %% md
-    # (3). Run CellStitch
-    # %%
+    selected_mask = np.zeros_like(cleaned_mask, dtype=bool)
+    rosette_region_coords = sorted_regions[0].coords
+    selected_mask[rosette_region_coords[:, 0], rosette_region_coords[:, 1], rosette_region_coords[:, 2]] = 1
 
-    print(f"Processing image {i + 1} over {len(images_path)}: {filename}")
+    selected_mask = cp.asnumpy(cuci_ski.transform.rescale(cp.array(selected_mask), 1/scale_factor, order=0))
 
-    print("computing xy masks")
-    xy_masks = model.eval(list(img), flow_threshold=flow_threshold, channels=[0, 0], diameter=xy_diameter)[0]
-    xy_masks = np.array(xy_masks)
-    print("xy masks computed")
-
-    print("computing yz masks")
-    yz_masks = model.eval(list(img.transpose(1, 0, 2)), flow_threshold=flow_threshold, channels=[0, 0],
-                                   diameter=z_diameter)[0]
-    yz_masks = np.array(yz_masks).transpose(1, 0, 2)
-    print("yz masks computed")
-
-    print("computing xz masks")
-    xz_masks = model.eval(list(img.transpose(2, 1, 0)), flow_threshold=flow_threshold, channels=[0, 0],
-                                   diameter=z_diameter)[0]
-    xz_masks = np.array(xz_masks).transpose(2, 1, 0)
-    print("xz masks computed")
-
-    print("Cellstitch combining masks")
-    cellstitch_masks = full_stitch(xy_masks, yz_masks, xz_masks)
-
-    # %% md
-    # (4). Save the Stitching results:
-    # %%
-    io.imsave(os.path.join(output_path, output_filename), cellstitch_masks)
-    print("Segmentation mask saved successfully")
-    # %% md
-print("Segmentation completed in {} seconds".format(time.time() - tic))
+    imsave(os.path.join(output_folder, fichier), selected_mask)
